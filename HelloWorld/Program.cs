68aa48a10d767122ab9866d03cc28eabb7ba78c3
James Cummings <james.r.cummings1@gmail.com>
2026-09-12 16:35:05 -0400

    G

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
+/*
+* Name: Your Full Name
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 8, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("James Cummings");
+Console.WriteLine("This is my first program at ETSU.");
